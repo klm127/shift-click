56f2b145a2e0bb45d010acb40cc5e30cfa8a38c5
klm127 <karlmiller127@gmail.com>
2024-06-03 12:11:35 -0400

    logo, rimraf

--- build/build.js
+++ build/build.js
@@ -1,8 +1,14 @@
 const { entries } = require("./entries");
 const build = require("vite").build
+const rimraf = require("rimraf").rimraf
 
+const feature_flags = require("./flags").default
 
 async function buildAll() {
+    if(feature_flags.Clean == true) {
+        console.log("Cleaning /dist.")
+        rimraf('dist')
+    }
     for(let [key, val] of Object.entries(entries)) {
         console.log(`Building ${key} from ${val}`)
         await build({
@@ -14,8 +20,7 @@ async function buildAll() {
                         dir: "dist",
                         entryFileNames: `js/${key}.js`,
                         assetFileNames: `assets/[name].[extname]`
-                    },
-                    
+                    },                    
                 },
                 
             }
--- build/flags.js
+++ build/flags.js
@@ -1,6 +1,8 @@
 
 const feature_flags = {
-
+    /** Whether to clean the dist directory. */
+    Clean: true 
 }
 
 exports.default = feature_flags
+
